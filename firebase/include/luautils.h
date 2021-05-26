f1e09d83b2d702ac35cf2490a92180d604ca14d5
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-05-26 14:37:33 +0200

    Added missing include

--- firebase/include/luautils.h
+++ firebase/include/luautils.h
@@ -7,3 +7,4 @@ void lua_pushtablestringstring(lua_State* L, const char* key, const char* value)
 void lua_pushtablestringfunction(lua_State* L, const char* key, lua_CFunction function);
 void lua_printstack(lua_State* L);
 void lua_setfieldstringstring(lua_State* L, const char* key, const char* value);
+void lua_setfieldstringnumber(lua_State* L, const char* key, const int value)
